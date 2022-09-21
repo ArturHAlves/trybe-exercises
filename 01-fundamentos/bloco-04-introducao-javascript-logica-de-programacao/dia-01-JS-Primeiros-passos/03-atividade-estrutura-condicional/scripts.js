51459527ede8d2e7dab1293b705680d5d226cdd7
Artur Henrique <arthenrdev@gmail.com>
2022-09-21 19:37:47 -0300

    Realização conteúdo

---
+++ 01-fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-JS-Primeiros-passos/03-atividade-estrutura-condicional/scripts.js
@@ -0,0 +1,14 @@
+const notaPessoaCandidata = 41;
+
+if (notaPessoaCandidata >= 80 && notaPessoaCandidata <= 100) {
+  console.log("Parábens, você foi aprovada(o)!")
+}
+
+else if (notaPessoaCandidata < 80 && notaPessoaCandidata >= 60) {
+  console.log("Você está na nossa lista de espera!")
+}
+
+else{
+  console.log("Você foi reprovado(a)!")
+}
+
