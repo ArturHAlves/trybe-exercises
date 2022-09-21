1d5c67400834f50546e6e6883840e5b5add34fc4
Artur Henrique <arthenrdev@gmail.com>
2022-09-21 15:30:55 -0300

    Atividade 2 concluída

---
+++ 01-fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-JS-Primeiros-passos/02-atividade-op-aritmeticos/scripts.js
@@ -0,0 +1,10 @@
+const base = 5;
+const heigth = 8;
+
+const area = base * heigth;
+
+const perimeter = base + heigth;
+
+// console.log(area);
+
+console.log(perimeter);
---
+++ 01-fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-JS-Primeiros-passos/02-utilizacao-typeof/scripts.js
@@ -0,0 +1,11 @@
+let patientId = '50'; //tipo string / QUANDO COLOCAMOS EM ASPA VIRA STRING
+let isEnrolled = true; //tipo boolean
+
+const patientInfo = { 
+  firstName: 'Ana', //tipo string
+  lastName: 'Santos', // tipo string
+};
+const patientEmail = 'ana@email.com'; //tipo string
+
+// console.log(typeof patientId) //string
+
