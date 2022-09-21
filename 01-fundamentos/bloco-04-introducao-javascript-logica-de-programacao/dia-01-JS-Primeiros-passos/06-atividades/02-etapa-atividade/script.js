45e17f14538b08ebfb2aee52328dea8d8129a1a0
Artur Henrique <arthenrdev@gmail.com>
2022-09-21 20:40:58 -0300

    Concluido tarefa 2

---
+++ 01-fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-JS-Primeiros-passos/06-atividades/02-etapa-atividade/script.js
@@ -0,0 +1,12 @@
+// Utilize if/else para fazer um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+
+const numero1 = 100;
+const numero2 = 100;
+
+if (numero1 > numero2) {
+  console.log("O numero1 é maior");
+} else if (numero1 < numero2) {
+  console.log("O número2 é maior");
+} else {
+  console.log("Os números são iguais");
+}
