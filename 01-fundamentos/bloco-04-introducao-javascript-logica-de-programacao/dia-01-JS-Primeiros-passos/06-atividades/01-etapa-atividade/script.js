91d890857a93b8fc9ee28b7a9e741e9c7287c6aa
Artur Henrique <arthenrdev@gmail.com>
2022-09-21 20:27:05 -0300

    Concluído tarefa 1

---
+++ 01-fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-01-JS-Primeiros-passos/06-atividades/01-etapa-atividade/script.js
@@ -0,0 +1,17 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados.
+
+const a = 5; 
+const b = 10; 
+
+let adicao = (a + b);
+
+let subtracao = (a - b);
+
+let multiplicacao = (a * b);
+
+let divisao = (a / b); 
+
+// console.log(adicao);
+// console.log(substracao);
+console.log(multiplicacao);
+// console.log(divisao);
