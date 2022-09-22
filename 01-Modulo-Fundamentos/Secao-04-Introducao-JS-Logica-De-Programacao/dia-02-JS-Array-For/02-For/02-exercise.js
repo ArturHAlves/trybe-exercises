44b967f41eb07e3120953e095848554b25ad01ae
Artur Henrique <arthenrdev@gmail.com>
2022-09-22 16:09:25 -0300

    Conteúdo dia

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/02-For/02-exercise.js
@@ -0,0 +1,18 @@
+//Imprimir os elementos da lista 
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0; index < groceryList.length; index += 1){
+  console.log(groceryList[index])
+}
+
+
+//Vai percorrer a lista e colocar o bem vindo para os respectivos nomes 
+
+// let convidados = ["Artur", "Pedro", "Lucas"]; 
+// let mensagem;
+
+// for(let index = 0; index < convidados.length; index += 1){
+//  mensagem = "Sejam bem vindos " + convidados[index] 
+//  console.log(mensagem)
+// }
