ca5aa075aea01e61eb3e9591a8913710d7549869
Artur Henrique <arthenrdev@gmail.com>
2022-09-22 15:33:52 -0300

    Conteúdo dia

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/01-Array/01-conteudo.js
@@ -0,0 +1,59 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// // let searchForFirstTask = tasksList[0];
+// // console.log(searchForFirstTask);
+// // // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa na primeira posição
+// console.log(tasksList);
+
+// ['Fazer exercícios da Trybe','Tomar café', 'Reunião', 'Brincar com o cachorro', ']
+
+
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+
+// // [ 'Tomar café', 'Reunião' ]
+
+
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.shift();  // remove a primeira tarefa
+// console.log(tasksList);
+
+// // ['Reunião', 'Brincar com o cachorro' ];
+
+
+
