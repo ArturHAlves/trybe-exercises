cdd63682d76211e5d339a3ff1ff2083d9132ba7c
Artur Henrique <arthenrdev@gmail.com>
2022-09-22 19:14:48 -0300

    Conteúdo do dia 2.0

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/04-While/01-conteudo.js
@@ -0,0 +1,13 @@
+// A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste for avaliada como verdadeira. A condição é avaliada antes da execução da rotina.
+
+// while (condição) {
+//   rotina
+// }
+
+// let n = 0;
+// let x = 0; 
+
+// while (n < 3) {
+//   n++;
+//   x +=n; 
+// }
