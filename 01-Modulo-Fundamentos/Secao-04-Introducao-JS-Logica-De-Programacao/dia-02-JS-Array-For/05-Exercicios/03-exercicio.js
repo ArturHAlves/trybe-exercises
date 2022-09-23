395aad2bc94fd9e9b6877292bc6a4fb62f293a66
Artur Henrique <arthenrdev@gmail.com>
2022-09-22 23:42:53 -0300

    Conclusão exercise

--- 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/05-Exercicios/03-exercicio.js
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/05-Exercicios/03-exercicio.js
@@ -4,10 +4,12 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-let media = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  soma = numbers[index] + numbers[index];
-  media = soma / numbers.length
-  console.log(media);
+  soma = soma + numbers[index];
 }
+
+let media = soma / numbers.length;
+console.log(media);
+
+
