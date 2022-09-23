2aa21faf9ecaff54e7ffdb92c9561d58d6082adc
Artur Henrique <arthenrdev@gmail.com>
2022-09-22 23:14:56 -0300

    Conclusão exercise

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/05-Exercicios/01-exercicio.js
@@ -0,0 +1,10 @@
+
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let index = 1; index < numbers; index+=1){
+  console.log(numbers[index]);
+
+}
---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/05-Exercicios/03-exercicio.js
@@ -0,0 +1,13 @@
+// Calcule e imprima a média aritmética dos valores contidos no array;
+
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma = numbers[index] + numbers[index];
+  media = soma / numbers.length
+  console.log(media);
+}
