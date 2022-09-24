034ae61c7e52b6a2505349f2a48d10d4d03c4df6
Artur Henrique <arthenrdev@gmail.com>
2022-09-24 20:48:26 -0300

    Conclusão exercise bonus JS - Primeiro passos

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-01-JS-Primeiros-passos/06-atividades/08-exercise-bonus.js
@@ -0,0 +1,19 @@
+
+// Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if
+
+const numero1 = 2;
+const numero2 = 1;
+const numero3 = 1;
+
+let par = true;
+
+if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0 ) {
+  console.log(par);
+}
+else{
+  console.log("false");
+}
+
+
+ 
