a6d44402246b5ca6a6fcfa43ed1c61babf7be010
Artur Henrique <arthenrdev@gmail.com>
2022-09-25 18:57:49 -0300

    Conclusão exercise JS - for

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-02-JS-Array-For/05-Exercicios/05-exercise.js
@@ -0,0 +1,12 @@
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = 0;
+
+
+for(let index = 0; index < numbers.length; index += 1){
+  if (numbers[index] > maiorNumero) {
+    maiorNumero = numbers[index]
+  }
+}
+console.log(maiorNumero);
