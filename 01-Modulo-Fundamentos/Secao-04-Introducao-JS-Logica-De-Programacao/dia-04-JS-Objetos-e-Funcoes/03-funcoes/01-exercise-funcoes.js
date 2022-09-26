99e2fc1f2ce3c7e70971e530c5d5e739e82a4639
Artur Henrique <arthenrdev@gmail.com>
2022-09-26 18:29:12 -0300

    Organiza conteúdo

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-04-JS-Objetos-e-Funcoes/03-funcoes/01-exercise-funcoes.js
@@ -0,0 +1,39 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+function adicao(n1, n2) {
+  return "Adição = " + (n1 + n2);
+}
+console.log(adicao(12, 12));
+
+
+function subtracao(n1, n2) {
+  return "Subtração = " + (n1 - n2);
+}
+console.log(subtracao(20, 12));
+
+
+
+function multiplicação(n1, n2) {
+  return "Multiplicação = " + n1 * n2;
+}
+console.log(multiplicação(5, 5));
+
+
+
+function divisao(n1,n2){
+  return "Divisão = " + n1 / n2;
+}
+console.log(divisao(12,2));
+
+
+
+function modulo(n1,n2){
+  return "Modúlo = " + n1 % n2;
+}
+console.log(modulo(55,2));
