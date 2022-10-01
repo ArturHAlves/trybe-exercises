801d02afcb24a3c5addcdc539f4eaaac97f550d5
Artur Henrique <arthenrdev@gmail.com>
2022-10-01 00:38:12 -0300

    Organiza diretórios dos dias

---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-03-JS-Logica-Programacao-algoritmos/04-exercise.js
@@ -0,0 +1,13 @@
+// Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
+let n = 5;
+let asteriscos = "*";
+let stringVazia = "";
+
+for(let index = 0; index < n; index += 1){
+  stringVazia += asteriscos
+  
+}
+for(let index = 0; index < n; index +=1){
+  console.log(stringVazia);
+}
---
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-03-JS-Logica-Programacao-algoritmos/05-exercise.js
@@ -0,0 +1,10 @@
+// Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let n = 5;
+let asteriscos = "*";
+let stringVazia = "";
+
+for(let index = 0; index < n; index +=1){
+  stringVazia += asteriscos
+  console.log(stringVazia);
+}
