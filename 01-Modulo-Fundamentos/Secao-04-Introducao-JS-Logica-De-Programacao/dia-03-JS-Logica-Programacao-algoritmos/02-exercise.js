f07e216e6dfd1c98bf679497ff1a8847f7c7b4f5
Artur Henrique <arthenrdev@gmail.com>
2022-09-30 23:34:13 -0300

    Organiza diretórios dos dias

--- 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-03-JS-Logica-Programacao-algoritmos/02-exercise.js
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-03-JS-Logica-Programacao-algoritmos/02-exercise.js
@@ -1,6 +1,6 @@
 // Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'artur';
+let word = 'Artur';
 
 let palavraInvertida = '';
 
--- 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-03-JS-Logica-Programacao-algoritmos/03-exercise.js
+++ 01-Modulo-Fundamentos/Secao-04-Introducao-JS-Logica-De-Programacao/dia-03-JS-Logica-Programacao-algoritmos/03-exercise.js
@@ -1,18 +1,22 @@
 // Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maiorPalavra = array[0]; 
-let menorPalavra = array[0];
+let lista = ['java', 'javascript', 'python', 'html', 'css'];
 
 
+let maiorPalavra = lista[0];
+let menorPalavra = lista[0];
+let contador = 0
 
+for(let index = 0; index < lista.length; index +=1){
+  if (lista[index].length > maiorPalavra.length) {
+    maiorPalavra = lista[index];
+    console.log(maiorPalavra);
+  }
+}
 
-
-
-
-// let palavra = 'tutu'
-// let numero = 0;
-// for(let index = 0; index < palavra.length; index+= 1){
-//  numero += index
-// }
-// console.log(numero);
+for(let index = 0; index < lista.length; index += 1){
+if (lista[index].length < menorPalavra.length) {
+  menorPalavra = lista[index]
+  console.log(menorPalavra);
+}
+}
