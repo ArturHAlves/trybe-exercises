e7d4f823dad1847edb131cb47db55728d410cae3
Artur Henrique <arthenrdev@gmail.com>
2022-10-03 16:01:14 -0300

    Organiza conteúdo dos dias

---
+++ 01-Modulo-Fundamentos/Secao-05-JS-Dom-Eventos-E-Web-Storage/dia-03-JS-Eventos/01-exercise/main.js
@@ -0,0 +1,54 @@
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+
+
+// - Copie esse arquivo e edite apenas ele; X
+//  - Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
+//Nõa sei 
+
+
+// - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+//  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+function mudarClasse(event){
+  let elementoTech = document.querySelector('.tech');
+elementoTech.classList.remove('tech')
+event.target.classList.add('tech')
+input.value = ''
+console.log(event.target);
+} 
+
+firstLi.addEventListener('click', mudarClasse);
+secondLi.addEventListener('click', mudarClasse);
+thirdLi.addEventListener('click', mudarClasse);
+
+
+// - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+// - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+//  - Que tal redirecionar para seu portfólio?
+
+// - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
