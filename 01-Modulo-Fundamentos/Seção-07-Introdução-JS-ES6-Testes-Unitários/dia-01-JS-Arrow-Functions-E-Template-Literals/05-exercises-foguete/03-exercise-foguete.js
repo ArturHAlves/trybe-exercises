14fbdaae990046b8a6de8d528b7dd14bdd72ce09
Artur Henrique <arthenrdev@gmail.com>
2022-10-19 08:47:57 -0300

    Organiza diretórios dos dias

---
+++ 01-Modulo-Fundamentos/Seção-07-Introdução-JS-ES6-Testes-Unitários/dia-01-JS-Arrow-Functions-E-Template-Literals/05-exercises-foguete/03-exercise-foguete.js
@@ -0,0 +1,9 @@
+// Crie a função factorial que recebe um número como parâmetro, utilizando arrow functions (Lembre-se de armazenar a função utilizando o tipo de variável correta).
+
+// Dentro da função crie uma variável result, que será o retorno final da função.
+
+// Crie a lógica para retornar o fatorial de N!.
+
+// Imprima no terminal “Esse é o fatorial resultado da função“ (Lembre-se de utilizar template literals nesse momento).
+
+// Bônus: tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
