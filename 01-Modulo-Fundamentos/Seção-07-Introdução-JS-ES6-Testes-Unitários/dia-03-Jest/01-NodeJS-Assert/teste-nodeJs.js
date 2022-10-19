98515f9f632d196f3e79e179fe4add1483faca32
Artur Henrique <arthenrdev@gmail.com>
2022-10-19 18:42:30 -0300

    Organiza diretórios dos dias

---
+++ 01-Modulo-Fundamentos/Seção-07-Introdução-JS-ES6-Testes-Unitários/dia-03-Jest/01-NodeJS-Assert/teste-nodeJs.js
@@ -0,0 +1,4 @@
+const assert = require('assert'); // Sintaxe para incluir o módulo assert
+
+assert.strictEqual(50, 50); // Sem erros: 50 === 50
+assert.strictEqual(50, 70); // AssertionError: 50 !== 70
