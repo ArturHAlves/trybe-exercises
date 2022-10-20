9b748d5f81fd028492a05a8ec30cd9bcfa0dd928
Artur Henrique <arthenrdev@gmail.com>
2022-10-20 09:01:13 -0300

    Organiza diretórios dos dias

---
+++ 01-Modulo-Fundamentos/Seção-07-Introdução-JS-ES6-Testes-Unitários/dia-02-JS-Fluxo-de-Excecao-E-Objetos/05-Object-entries/01-exemplo.js
@@ -0,0 +1,10 @@
+const coolestTvShow = {
+  name: 'BoJack Horseman',
+  genre: 'adult animation',
+  createdBy: 'Raphael Bob-Waksberg',
+  favoriteCharacter: 'Princess Carolyn',
+  quote: 'Princess Carolyn always lands on her feet.',
+  seasons: 6,
+};
+
+console.log(Object.entries(coolestTvShow));
---
+++ 01-Modulo-Fundamentos/Seção-07-Introdução-JS-ES6-Testes-Unitários/dia-02-JS-Fluxo-de-Excecao-E-Objetos/06-Object-assign/02-exemplo.js
@@ -0,0 +1,12 @@
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
