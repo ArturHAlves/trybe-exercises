b68a7fdf6dd46e50d3130880c6d2eba90fa1e644
Artur Henrique <arthenrdev@gmail.com>
2022-11-09 09:39:00 -0300

    Atualizando gerador de senha

--- 02-Modulo-Front-End/Secao-01-Front-End-E-JS-Assincrono/dia-01-Ambiente-de-Desenvolvimento/01-Gerador-de-Senha/src/main.js
+++ 02-Modulo-Front-End/Secao-01-Front-End-E-JS-Assincrono/dia-01-Ambiente-de-Desenvolvimento/01-Gerador-de-Senha/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import "./style.css"
+import copy from 'clipboard-copy'
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -8,3 +9,8 @@ passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
 });
+
+displayPasswordEl.addEventListener('click', (event) => {
+copy(event.target.innerHTML);
+alert('Copiado com Sucesso')
+})
