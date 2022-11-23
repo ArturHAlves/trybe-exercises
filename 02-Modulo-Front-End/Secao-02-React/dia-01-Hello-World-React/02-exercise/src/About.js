bbf35fbcd284afe79214eeec9941ad51ded94dd3
Artur Henrique <arthenrdev@gmail.com>
2022-11-22 22:39:27 -0300

    Realização 02-exercise

---
+++ 02-Modulo-Front-End/Secao-02-React/dia-01-Hello-World-React/02-exercise/src/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Artur Henrique</h1>
+        <p>Aprendendo programar</p>
+
+        <h2>Minhas Habilidades:</h2>
+        <ol>
+          <li>JS</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>React</li>
+        </ol>
+      </div>
+    )
+  }
+}
+
+export default About;
