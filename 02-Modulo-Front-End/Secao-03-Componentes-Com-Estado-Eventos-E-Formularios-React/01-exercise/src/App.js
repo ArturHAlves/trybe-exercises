3fded82c4f24aa40347a3c1306c6bf48fa0f68d0
Artur Henrique <arthenrdev@gmail.com>
2022-11-29 19:04:20 -0300

    Alterações no 01-exericse

--- 02-Modulo-Front-End/Secao-03-Componentes-Com-Estado-Eventos-E-Formularios-React/01-exercise/src/App.js
+++ 02-Modulo-Front-End/Secao-03-Componentes-Com-Estado-Eventos-E-Formularios-React/01-exercise/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  eventoBotao() {
+    constructor() {
+      super()
+
+      this.eventoBotao = this.eventoBotao.bind(this)
+      // this.eventoBotao2 = this.eventoBotao2.bind(this)
+      this.eventoBotao3 = this.eventoBotao3.bind(this)
+
+    }
+  
+  // Como o eventoBotao está usando o arrow function, não tem a necessidade de usar bind 
+  eventoBotao = () => {
     console.log('Botão1');
+    console.log(this);
   }
 
+  // como o eventoBotao2 não está usando arrow function, precisa de todo o constructor e o bind 
   eventoBotao2() {
     console.log('Botão2');
+    console.log(this);
   }
 
   eventoBotao3() {
