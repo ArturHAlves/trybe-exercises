3dc93512869d5db6da89129b675746b63f74b125
Artur Henrique <arthenrdev@gmail.com>
2022-11-29 23:00:29 -0300

    Alterações 01-exercise

--- 02-Modulo-Front-End/Secao-03-Componentes-Com-Estado-Eventos-E-Formularios-React/01-exercise/src/App.js
+++ 02-Modulo-Front-End/Secao-03-Componentes-Com-Estado-Eventos-E-Formularios-React/01-exercise/src/App.js
@@ -1,37 +1,26 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-    constructor() {
-      super()
+  constructor() {
+    super();
 
-      this.eventoBotao = this.eventoBotao.bind(this)
-      // this.eventoBotao2 = this.eventoBotao2.bind(this)
-      this.eventoBotao3 = this.eventoBotao3.bind(this)
-
-    }
-  
-  // Como o eventoBotao está usando o arrow function, não tem a necessidade de usar bind 
-  eventoBotao = () => {
-    console.log('Botão1');
-    console.log(this);
+    this.eventoBotao = this.eventoBotao.bind(this);
   }
 
-  // como o eventoBotao2 não está usando arrow function, precisa de todo o constructor e o bind 
-  eventoBotao2() {
-    console.log('Botão2');
-    console.log(this);
-  }
+  state = {
+    numeroDeCliques: 0,
+  };
 
-  eventoBotao3() {
-    console.log('Botão3');
+  eventoBotao() {
+    this.setState((valorAtual) => ({
+      numeroDeCliques: valorAtual.numeroDeCliques + 1,
+    }));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.eventoBotao}>Botão 1</button>
-        <button onClick={this.eventoBotao2}>Botão 2</button>
-        <button onClick={this.eventoBotao3}>Botão 3</button>
+        <button onClick={this.eventoBotao}>{this.state.numeroDeCliques}</button>
       </div>
     );
   }
