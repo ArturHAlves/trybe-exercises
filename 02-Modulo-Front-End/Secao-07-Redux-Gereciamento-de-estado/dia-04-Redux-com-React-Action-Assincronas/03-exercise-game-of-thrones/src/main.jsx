60eef60d8e6d2000d78067c7430b3c3c86512911
Artur Henrique <arthenrdev@gmail.com>
2023-02-02 11:19:47 -0300

    [UPDATE] Add exercise - Action Assincronas

---
+++ 02-Modulo-Front-End/Secao-07-Redux-Gereciamento-de-estado/dia-04-Redux-com-React-Action-Assincronas/03-exercise-game-of-thrones/src/main.jsx
@@ -0,0 +1,13 @@
+// ./src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
