ce07d70eda592fd5bf1c2afc0a182f07992d9a3e
Artur Henrique <arthenrdev@gmail.com>
2023-07-05 16:15:18 -0300

    feat: Exercício - Herança e Composição

--- 03-Modulo-Back-End/Secao-09-POO-E-SOLID/dia-02-Heranca-e_Composicao/04-Composicao-e-Agregacao/02-agregacao.ts
+++ 03-Modulo-Back-End/Secao-09-POO-E-SOLID/dia-02-Heranca-e_Composicao/04-Composicao-e-Agregacao/02-agregacao.ts
@@ -1,35 +1,35 @@
-class Friend {
-  private nickname: string;
+// class Friend {
+//   private nickname: string;
 
-  public getNickname(): string {
-    return this.nickname;
-  }
+//   public getNickname(): string {
+//     return this.nickname;
+//   }
 
-  public setNickname(nickname: string) {
-    this.nickname = nickname;
-  }
-}
+//   public setNickname(nickname: string) {
+//     this.nickname = nickname;
+//   }
+// }
 
-class SocialMediaAccount {
-  private friends = new Array<Friend>();
+// class SocialMediaAccount {
+//   private friends = new Array<Friend>();
 
-  public addFriend(friend: Friend) {
-    this.friends.push(friend);
-  }
+//   public addFriend(friend: Friend) {
+//     this.friends.push(friend);
+//   }
 
-  public printFriends() {
-    this.friends.map((friend) => console.log(friend));
-  }
-}
+//   public printFriends() {
+//     this.friends.map((friend) => console.log(friend));
+//   }
+// }
 
-const socialMediaAccount = new SocialMediaAccount();
+// const socialMediaAccount = new SocialMediaAccount();
 
-const friendCarol = new Friend();
-friendCarol.setNickname('Carol');
+// const friendCarol = new Friend();
+// friendCarol.setNickname('Carol');
 
-const friendFernando = new Friend();
-friendFernando.setNickname('Fernando');
+// const friendFernando = new Friend();
+// friendFernando.setNickname('Fernando');
 
-socialMediaAccount.addFriend(friendCarol);
-socialMediaAccount.addFriend(friendFernando);
-socialMediaAccount.printFriends();
+// socialMediaAccount.addFriend(friendCarol);
+// socialMediaAccount.addFriend(friendFernando);
+// socialMediaAccount.printFriends();
