182c0186a36e71e176936151ea869f09d8f5c71f
Artur Henrique <arthenrdev@gmail.com>
2023-07-07 12:20:58 -0300

    feat: Arquivos exemplos Polimorfismo e prática

---
+++ 03-Modulo-Back-End/Secao-09-POO-E-SOLID/dia-03-Polimorfismo/02-sintaxe/02-Sintaxe-com-Classe.ts
@@ -0,0 +1,28 @@
+class Animal {
+  constructor(public name: string) { }
+  move() { console.log(`${this.name} está se movendo.`); }
+}
+class Bird extends Animal {
+  move() { console.log(`${this.name} está voando.`); }
+}
+class Mammal extends Animal {
+  move() { console.log(`${this.name} está andando.`); }
+}
+
+const a = new Animal('Tubarão');
+const b = new Bird('Papagaio');
+const m = new Mammal('Tatu');
+
+const myMove = (animal: Animal) => {
+  animal.move();
+}
+myMove(a);
+myMove(b);
+myMove(m);
+
+/*
+Saída:
+Tubarão está se movendo.
+Papagaio está voando.
+Tatu está andando.
+*/
