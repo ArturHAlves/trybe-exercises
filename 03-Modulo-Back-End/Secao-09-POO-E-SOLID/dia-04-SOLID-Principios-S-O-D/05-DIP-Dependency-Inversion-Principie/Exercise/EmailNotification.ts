e10b0282f60a73f44ea0c73bbeab03b62bb73c67
Artur Henrique <arthenrdev@gmail.com>
2023-07-14 16:07:54 -0300

    feat: Arquivo DIP

---
+++ 03-Modulo-Back-End/Secao-09-POO-E-SOLID/dia-04-SOLID-Principios-S-O-D/05-DIP-Dependency-Inversion-Principie/Exercise/EmailNotification.ts
@@ -0,0 +1,18 @@
+// EmailNotification.ts
+
+import Notificator from './Notificator';
+
+export default class EmailNotification implements Notificator {
+  private email: string;
+
+  constructor(email: string) {
+    this.email = email;
+  }
+
+  sendNotification(message: string): void {
+    console.log(
+      `Here should go the implementation \
+      to send notification to the email: ${this.email} - ${message}`,
+    );
+  }
+}
